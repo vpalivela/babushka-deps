f59bc14ec44580a46527230997c0ef472ac20734
Venkat Palivela <venkat.palivela@gmail.com>
2014-07-12 12:42:41 -0500

    fixed git autocompletion url issue

--- git.rb
+++ git.rb
@@ -1,10 +1,10 @@
 dep 'git autocompletion' do
-  # requires 'git.bin'
+  requires 'git.bin'
 
   met? { "~/.git-completion.bash".p.exists? }
 
   meet {
-    log_shell "Downloading git-completion.bash", "curl https://raw.github.com/git/git/master/contrib/completion/git-completion.bash -o $HOME/.git-completion.bash"
+    log_shell "Downloading git-completion.bash", "curl -o ~/.git-completion.bash https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash"
     log_shell "Putting it into .bash_profile", "echo '
 # Git Autocompletion
 if [ -f ~/.git-completion.bash ]; then
