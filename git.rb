42975b6b0cc79601ff9114086eaee3f43cd4d883
Venkat Palivela <venkat.palivela@gmail.com>
2014-07-12 12:34:37 -0500

    tweaking git autocompletion

--- git.rb
+++ git.rb
@@ -1,11 +1,12 @@
 dep 'git autocompletion' do
-  requires 'git.src'
+  # requires 'git.bin'
 
   met? { "~/.git-completion.bash".p.exists? }
 
   meet {
     log_shell "Downloading git-completion.bash", "curl https://raw.github.com/git/git/master/contrib/completion/git-completion.bash -o $HOME/.git-completion.bash"
-    log_shell "Putting it into .bash_profile", "echo '# Git Autocompletion
+    log_shell "Putting it into .bash_profile", "echo '
+# Git Autocompletion
 if [ -f ~/.git-completion.bash ]; then
   . ~/.git-completion.bash
 fi' >> $HOME/.bash_profile"
