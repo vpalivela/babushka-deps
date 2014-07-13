534e63ac01cc6669593bcc97d9eab4abe9232846
Venkat Palivela <venkat.palivela@gmail.com>
2014-07-13 13:21:48 -0500

    Updates and reorg of deps

--- gems.rb
+++ gems.rb
@@ -2,12 +2,6 @@ dep "compass.gem" do
   installs "compass"
 end
 
-dep "cocoapods.gem" do
-  installs "cocoapods"
-  provides "pod"
-end
-
 dep "gems" do
   requires "compass.gem"
-  requires "cocoapods.gem"
 end
--- rvm.rb
+++ rvm.rb
@@ -4,8 +4,8 @@ dep 'rvm' do
   }
 
   meet {
-    shell '\curl -sSL https://get.rvm.io | bash -s stable --autolibs=homebrew --ruby'
-    shell 'bash -c "source ~/.rvm/scripts/rvm; rvm notes"'
+    log_shell "Installing rvm", '\curl -sSL https://get.rvm.io | bash -s stable --autolibs=homebrew --ruby'
+    log_shell "Reloading rvm", 'bash -c "source ~/.rvm/scripts/rvm; rvm notes"'
     shell "echo \"
 # RVM
 [[ -s '/Users/`whoami`/.rvm/scripts/rvm' ]] && source '/Users/`whoami`/.rvm/scripts/rvm'\" >> /etc/bashrc
