ff82fe15927823414f1da9ebae5fc178a5c6551b
Venkat Palivela <venkat.palivela@gmail.com>
2014-07-13 09:57:07 -0500

    Added application deps

---
+++ apps.rb
@@ -0,0 +1,18 @@
+dep 'Google Chrome.app' do
+  source 'http://dl.google.com/chrome/mac/dev/GoogleChrome.dmg'
+end
+
+dep 'Alfred.app' do
+  source 'http://cachefly.alfredapp.com/Alfred_2.3_264.zip'
+end
+
+dep 'iTerm.app' do
+  source 'http://www.iterm2.com/downloads/stable/iTerm2_v1_0_0.zip'
+end
+
+dep 'SourceTree.app' do
+  source 'http://downloads.atlassian.com/software/sourcetree/SourceTree_1.9.5.2.dmg'
+end
+
+dep 'Atom.app' do
+  source 'https://atom.io/download/mac'
